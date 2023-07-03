95174ac9a2263717e99bfbaa41facb8c420dfc9d
at-elcapitan <gameforceman1808@gmail.com>
2023-07-03 21:33:39 +0300

    pda-030720232133

--- pda/__main__.py
+++ pda/__main__.py
@@ -1,6 +1,7 @@
 import os
 import json
 import asyncio
+import threading
 
 import hikvisionapi as hikv
 from dotenv import load_dotenv
@@ -10,15 +11,25 @@ from telebot import types
 # Globals
 load_dotenv()
 TOKEN = os.getenv('TOKEN')
+HOST = os.getenv('HOST')
+LOGIN = os.getenv('LOGIN')
+PASSWD = os.getenv('PASSWD')
 bot = AsyncTeleBot(TOKEN)
 
 
+class EnvironmentError(Exception):
+    def __init__(self) -> None:
+        super().__init__(f'Can`t import global variables. Maybe .env file corrupted or filled incorrectly.')
+
+
 def main():
-    hikv.AsyncClient()
+    aclient = hikv.Client(HOST, LOGIN, PASSWD, timeout=5)
+
+    aclient.System.deviceInfo(method='get')
 
 
 @bot.message_handler(commands=['help', 'start'])
-async def send_welcome(message):
+async def send_welcome(message) -> None:
     with open('files/allowed_chats.json', "r") as f:
         data = json.load(f)
 
@@ -29,8 +40,34 @@ async def send_welcome(message):
     await bot.reply_to(message, f"Фатальная ошибка: данный чат не добавлен в вайтлист.\nИдентефикатор чата: `{message.chat.id}`\nНапишите @takhiga для добавления в вайтлист.", parse_mode="Markdown")
 
 
+@bot.message_handler(commands=['addchat'])
+async def add_chat(message) -> None:
+    if message.from_user.id in [1202182074]:
+        await bot.reply_to(message, f"Запрос авторизирован. Добро пожаловать в систему, @{message.from_user.username}\n\nПровожу регистрацию чата...")
+
+        with open('files/allowed_chats.json', 'r+') as f:
+            data = json.load(f)
+
+            if message.chat.id in data:
+                await bot.send_message(message.chat.id, "Ошибка: чат уже находится в вайтлисте.")
+                return
+            
+            data.append(message.chat.id)
+            f.seek(0)
+            json.dump(data, f, indent=4, ensure_ascii=False)
+            f.truncate()
+        
+        await bot.send_message(message.chat.id, "Выполнено.")
+        return
+    
+    await bot.reply_to(message, "Фатальная ошибка авторизации.")
+
+
 if __name__ == "__main__":
-    print("pda-03072023")
+    print("pda-030720232133 Starting...")
+    if TOKEN is None or PASSWD is None or HOST is None or LOGIN is None:
+        raise EnvironmentError
+
     if not os.path.exists('files'):
         os.mkdir('files')
 
@@ -41,5 +78,12 @@ if __name__ == "__main__":
             json.dump(def_config, f, indent=4, ensure_ascii=False)
             f.truncate()
 
-    asyncio.run(bot.polling())
-    main()
+    print("Creating threads")
+    thread = threading.Thread(target=asyncio.run, args=(bot.polling(),))
+    thread_main = threading.Thread(target=main)
+    print("Starting...")
+    thread_main.start()
+    thread.start()
+    print("Started.")
+    thread_main.join()
+    thread.join()
